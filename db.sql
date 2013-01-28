5750ef51c0fff4190c60cf064124833a6980399b
svevad <svevad@gmail.com>
2013-01-28 18:17:30 +0000

    fiksa sql-script

--- db.sql
+++ db.sql
@@ -22,10 +22,15 @@ CREATE TABLE blogg (
 	bloggID int(32) NOT NULL AUTO_INCREMENT, 
 	brukerID int(32) NOT NULL, 
 	brukernivaa varchar(16) NOT NULL, 
+<<<<<<< .mine
+	tittel varchar(32) NOT NULL, 
+	beskrivelse varchar(20000),
+=======
 	tittel varchar(32) NOT NULL, 
 	beskrivelse varchar(MAX),
+>>>>>>> .r11
 	PRIMARY KEY (bloggID),
-	FOREIGN KEY (brukerID) REFERENCES bruker(brukerID),
+	FOREIGN KEY (brukerID) REFERENCES bruker(brukerID)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
@@ -36,7 +41,7 @@ CREATE TABLE bloggpost (
 	timestamp int(10) NOT NULL, 
 	readcount int(128) NOT NULL, 
 	kommentarcound int(128) NOT NULL,
-	posttekst varchar(MAX) NOT NULL,
+	posttekst varchar(20000) NOT NULL,
 	PRIMARY KEY(postID),
 	FOREIGN KEY(bloggID) REFERENCES blogg(bloggID),
 	FOREIGN KEY(brukerID) REFERENCES bruker(brukerID)
@@ -49,10 +54,10 @@ CREATE TABLE kommentar  (
 	bloggpostID int(32) NOT NULL, 
 	timestamp int(10) NOT NULL, 
 	source varchar(256) NULL,
-	kommentar varchar(MAX),
+	kommentar varchar(20000),
 	PRIMARY KEY (kommentarID),
-	FOREIGN KEY brukerID REFERENCES bruker(brukerID),
-	FOREIGN KEY bloggpostID REFERENCES bloggpost(postID)
+	FOREIGN KEY (brukerID) REFERENCES bruker(brukerID),
+	FOREIGN KEY (bloggpostID) REFERENCES bloggpost(postID)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE bilde (
@@ -61,5 +66,5 @@ CREATE TABLE bilde (
 	url varchar(1024) NOT NULL,
 	timestamp int(10) NOT NULL,
 	PRIMARY KEY (bildeID),
-	FOREIGN KEY brukerID REFERENCES bruker(brukerID)
+	FOREIGN KEY (brukerID) REFERENCES bruker(brukerID)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
