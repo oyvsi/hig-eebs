ef7276f4b2f8330361ab49b534c33e8190e26fdf
newsflash@gmail.com <newsflash@gmail.com@76afb865-2419-4a4a-d925-d84ee1375376>
2013-01-29 17:57:12 +0000

    Clean up some. Add autoloading

--- trunk/application/libs/BaseController.php
+++ trunk/application/libs/BaseController.php
@@ -3,6 +3,7 @@
 abstract class BaseController {
 	protected $args;
 	protected $view;
+	protected $model;
 
 	public function __construct() {
 		$this->view = new View();
--- trunk/includes/init.php
+++ trunk/includes/init.php
@@ -1,21 +1,25 @@
 <?php
- include __SITE_PATH . '/application/' . 'conf/database.php';
- include __SITE_PATH . '/application/' . 'libs/baseModel.php';
- include __SITE_PATH . '/application/' . 'libs/view.php';
- include __SITE_PATH . '/application/' . 'libs/baseController.php';
- include __SITE_PATH . '/application/' . 'helpers/html.php';
- include __SITE_PATH . '/application/bootstrap.php';
+include __SITE_PATH . '/application/bootstrap.php';
 
- /*** auto load model classes ***/
-    function __autoload($class_name) {
-    $filename = strtolower($class_name) . '.class.php';
-    $file = __SITE_PATH . '/model/' . $filename;
+/**
+* Autoloads the application's classfiles
+* Is automagically called by php
+*
+* @param string $className - The name of the class  
+*/
+function __autoload($className) {
+	$paths = array(array('path' => __SITE_PATH . '/application/controllers/', 'postfix' => '.php'),
+				array('path' => __SITE_PATH . '/application/models/', 'postfix' => '.php'),
+				array('path' => __SITE_PATH . '/application/libs/', 'postfix' => '.php'),
+				array('path' => __SITE_PATH . '/application/helpers/', 'postfix' => '.php'));
 
-    if (file_exists($file) == false)
-    {
-        return false;
-    }
-  include ($file);
+	foreach($paths as $path) {
+		$filename = $path['path'] . $className . $path['postfix'];
+		if(is_readable($filename)) {
+			include($filename);
+			return true;	
+		} 
+	}
 }
 
 ?>
