365b3be0839bf3d6e76c65f768745c3660c0db34
newsflash@gmail.com <newsflash@gmail.com@76afb865-2419-4a4a-d925-d84ee1375376>
2013-01-29 17:00:42 +0000

    Fixed an error in link

--- trunk/application/helpers/html.php
+++ trunk/application/helpers/html.php
@@ -2,6 +2,6 @@
 
 class HTML {
 	public static function appLink($desc, $link) {
-		return '<a href="' . $_SERVER['HTTP_HOST'] . $link . '">' . $desc . '</a>'; 
+		return '<a href="http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $link . '/">' . $desc . '</a>'; 
 	}
 }
