80106092648ec21ac4e0268160e1f7e85970a850
svevad@gmail.com <svevad@gmail.com@76afb865-2419-4a4a-d925-d84ee1375376>
2013-01-29 21:55:46 +0000

    prøver meg på wysiwyg

---
+++ trunk/wysiwyg/index.php
@@ -0,0 +1,28 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+    <script type="text/javascript" src="jHtmlArea-0.7.5.js"></script>
+    <link rel="Stylesheet" type="text/css" href="jHtmlArea.css" />
+    
+    <style type="text/css">
+        /* body { background: #ccc;} */
+        div.jHtmlArea .ToolBar ul li a.custom_disk_button 
+        {
+            background: url(images/disk.png) no-repeat;
+            background-position: 0 0;
+        }
+        
+        div.jHtmlArea { border: solid 1px #ccc; }
+    </style>
+</head>
+<body>
+<script type="text/javascript" src="jHtmlArea-0.7.5.min.js">
+	$(function(){
+		$("textarea").htmlarea({
+			css: "style//jHtmlArea.Editor.css"
+		});
+	});
+</script>	
+
+</body>
+</html>
