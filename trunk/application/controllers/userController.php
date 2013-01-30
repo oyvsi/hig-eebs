5b806657d3344c651fa96ec710a2faff24360050
bundy.strand <bundy.strand@76afb865-2419-4a4a-d925-d84ee1375376>
2013-01-30 18:53:16 +0000

    UserShell

--- trunk/application/controllers/userController.php
+++ trunk/application/controllers/userController.php
@@ -2,6 +2,28 @@
 class UserController extends BaseController {
 
 	public function __construct() {
-		echo "UserController here";
+		parent::__construct();
+		echo "UserController here<br>\n<br>\n";
 	}
+
+	public function createAccount() {
+		echo "Create me an blugg";
+	}
+
+	public function resetPassword() {
+		echo "Forgot pw =( or got hackedlol";
+	}
+
+	public function editInformation() {
+		echo "I have aged or become a girl";
+	}
+
+	public function changeDisplayName() {
+		echo "Ive hated on some peeps";
+	}
+
 }
+
+
+
+
