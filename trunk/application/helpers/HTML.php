42439b557466a128abd9044d10ed2f64aadd174f
newsflash@gmail.com <newsflash@gmail.com@76afb865-2419-4a4a-d925-d84ee1375376>
2013-01-31 22:17:36 +0000

    Started work on frontpage

--- trunk/application/helpers/HTML.php
+++ trunk/application/helpers/HTML.php
@@ -1,7 +1,11 @@
 <?php
 
 class HTML {
-	public static function appLink($desc, $link) {
-		return '<a href="http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $link . '/">' . $desc . '</a>'; 
+	public static function appLink($link, $desc) {
+		return '<a href="http://' . __URL_PATH . $link . '/">' . $desc . '</a>'; 
+	}
+	public static function cssLink($name) {
+		return '<link rel="stylesheet" type="text/css" href="' . $_SERVER['REQUEST_URI'] . 'media/style/' . $name . '.css" />';
 	}
 }
+
--- trunk/application/views/footer.php
+++ trunk/application/views/footer.php
@@ -1,3 +1,4 @@
-	2013 - Laffen, Bundy, Flash & PK
+	<div id="footer">2013 - Laffen, Bundy, Flash & PK</div>
+	</div>
 	</body>
 </html>
