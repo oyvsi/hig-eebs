acddc6f6212ffa94a6cab383ba3edc1f82aa6dcb
newsflash <newsflash@gmail.com>
2013-02-01 19:48:25 +0000

    Added some menu items. There's still a routing issue for index functions

--- application/views/header.php
+++ application/views/header.php
@@ -4,4 +4,17 @@
 	<?php echo HTML::cssLink('blog'); ?>
 	</head>
 	<body>
+	<div id="navigation">
+		<ul>
+			<?php
+			echo '<li>' . HTML::appLink('index/mostRead', 'Most read posts') . '</li>';
+			echo '<li>' . HTML::appLink('index/mostCommented', 'Most commented posts') . '</li>';
+			if(Auth::checkLogin()): 
+				echo '<li>' . HTML::appLink('blog/post', 'Create new post') . '</li>';
+				echo '<li>' . HTML::appLink('user/profile/usernamevar', 'Profile') . '</li>';
+				echo '<li>' . HTML::appLink('user/logOut', 'Logg out') . '</li>';
+			endif;
+			?>
+		</ul>
+	</div>
 	<div id="contents">
