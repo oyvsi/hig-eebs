7b03fde8e783de4f89c7ce6e60671f5ad179026f
newsflash@gmail.com <newsflash@gmail.com@76afb865-2419-4a4a-d925-d84ee1375376>
2013-02-01 11:59:30 +0000

    added function for SEO-friendly urls. No idea where it belongs

--- trunk/application/controllers/BlogpostController.php
+++ trunk/application/controllers/BlogpostController.php
@@ -13,16 +13,37 @@ class BlogpostController extends BaseController {
 		echo 'Show a post';
 	}
 	public function create() {
-		//maybe in constructor?
-		echo 'Get me that wyziwig';
+		echo 'Creating post...';
+		$title = 'Arne dro fisken på land!';
+		echo 'URL: ' makePostUrl($title);	// TODO: Make postURL in db as well
+
 	}
+	
+	/**
+	 * Creates pretty URL from string
+	 * Covertes spaces to underscore and replaces NO-specific characters
+	 * everything but alphanumeric is stripped after replace
+	 * 
+	 * @param String $title the string to convert
+	 */
+	public function makePostUrl($title) {
+		$title = strtolower($title);
+		$replace = array(' ' => '_', 'æ' => 'ae', 'ø' => 'oe', 'å' => 'aa');
+		foreach($replace as $char => $sub) {
+			$title = str_replace($char, $sub, $title);
+		}
+		$title = preg_replace('/[^0-9a-z_]/', "", $title);
+
+		return $title;
+	}
+
 	public function update() {
 		echo 'add some shit';
 	}
 	public function delete() {
 		echo 'Well this sucked, remove it!';
 	}
-	
+
 	public function flag() {
 		echo 'Mark no-good';
 	}
