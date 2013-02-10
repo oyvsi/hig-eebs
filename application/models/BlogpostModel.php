8d4ecf9f620cc751537d0042a72b31012c99a536
newsflash <newsflash@gmail.com>
2013-02-10 19:23:33 +0000

    Moved stuff from BlogController to BlogpostController

--- application/models/BlogpostModel.php
+++ application/models/BlogpostModel.php
@@ -5,9 +5,38 @@ class BlogpostModel extends BaseModel {
 		parent::__construct();
 	}	
 
+	public function getPost($blogName, $postURL) {
+		$userID = $this->db->select('SELECT userID from users WHERE userName = :userName', array(':userName' => $blogName));
+		$query = 'SELECT blogPosts.*, count(comments.commentID) as noComments FROM blogPosts 
+						LEFT JOIN comments ON comments.postID = blogPosts.postID	
+						WHERE blogPosts.postURL = :postURL AND blogPosts.userID = :userID';
+		$result = $this->db->select($query, array('postURL' => $postURL, 'userID' => $userID[0]['userID']));
+		$result[0]['userName'] = $blogName;
+		$this->setInfo($result[0]);
+
+		return $result;
+	}
+
+	public function updatePostViewCount($postID) {
+		// Maybe do this with cookies
+		$ipAddress = BlogModel::getRealIpAddress();
+		$reReadLimit = 24;
+		$limitTime = strtotime('-' . $reReadLimit . ' hours');
+		$check = $this->db->select('SELECT viewID FROM postViews 
+			WHERE postID = :postID AND ipAddress = :ipAddress AND timestamp BETWEEN :startTime AND :stopTime',
+			array(':postID' => $postID, ':ipAddress' => $ipAddress, ':startTime' => $limitTime, ':stopTime' => time()));
+
+		// User has not seen this post yet, or not since timelimit. Insert a post view.
+		if(count($check) == 0) {
+			$query = 'INSERT INTO postViews(postID, timestamp, ipAddress) VALUES (:postID, :timestamp, :ipAddress)';
+			$values = array(':postID' => $postID, ':timestamp' => time(), 'ipAddress' => $_SERVER['REMOTE_ADDR']); 
+			$this->db->insert($query, $values);
+		}
+	}
+
 	public function createPost($data, $userID) {
 		// Do some validation shit and check for XSS
-		
+
 		$url = $this->makePostUrl($data['title']);
 		$title = $data['title'];
 		$contents = $data['postText'];
@@ -35,5 +64,4 @@ class BlogpostModel extends BaseModel {
 
 		return $title;
 	}
-
 }
