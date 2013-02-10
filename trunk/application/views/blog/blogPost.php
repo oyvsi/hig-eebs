57eda7023ed6ff80669cf296394ba791256a11dd
newsflash@gmail.com <newsflash@gmail.com@76afb865-2419-4a4a-d925-d84ee1375376>
2013-02-10 22:39:26 +0000

    ..., Jessica

---
+++ trunk/application/views/blog/blogPost.php
@@ -0,0 +1,14 @@
+<?php
+// TODO: This is a massive duplicate of blogPosts. Fix.
+//print_r($this->vars);
+foreach($this->vars['blogPosts'] as $blogPost) {
+	extract($blogPost);
+	echo '<div class="blogPostSummary" id="' . $postID . '">';
+	echo '<div class="blogPostTitle"><h1>' . $postTitle . '</h1></div>';
+	echo '<div class="blogPostText">' . $postText . '</div>';
+	echo '<div class="blogPostFooter"><p class="commentsLink">';
+	echo '<a href="comments">' . $noComments . ' comment' . ($noComments != 1 ? 's' : '') . '</a>';
+	echo '</p><p class="byLine">By ' . HTML::appLink('user/profile/' . $userName, $userName) . ' ' . date('d.m.Y H:i', $timestamp) . '</p>';
+	echo '<div style="clear: both;"></div></div></div>';
+	
+}
