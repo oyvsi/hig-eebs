f5cf917f57f584a20bde2829136c614eb9b69f5d
newsflash <newsflash@gmail.com>
2013-02-11 10:42:15 +0000

    Refactor IndexModel a bit

--- application/models/IndexModel.php
+++ application/models/IndexModel.php
@@ -1,40 +1,29 @@
 <?php
 
 class IndexModel extends BaseModel {
+	private $baseQuery = 'SELECT LEFT(blogPosts.postText, 60) as postSummary, blogPosts.*, users.userName, 
+								COUNT(postViews.postID) as readCount, COUNT(comments.postID) as noComments FROM blogPosts 
+					 			LEFT JOIN postViews ON postViews.postID = blogPosts.postID 
+					 			LEFT JOIN users ON blogPosts.userID = users.userID
+					 			LEFT JOIN comments ON comments.PostID = blogPosts.postID';
+	
 	public function __construct() {
 		parent::__construct();
 	}
 
 	public function lastPosts($limit) {
-		return $this->db->select('SELECT LEFT(postText, 60) as postSummary, blogPosts.*, users.userName, COUNT(comments.commentID) as noComments
-											FROM blogPosts 
-											LEFT JOIN users on blogPosts.userID = users.UserID 
-											LEFT JOIN comments on comments.postID = blogPosts.postID 
-											GROUP BY blogPosts.postID
-											ORDER BY timestamp DESC LIMIT :limit', 
-											array(':limit'=> 10));
+		return $this->db->select($this->baseQuery . ' GROUP BY blogPosts.postID ORDER BY timestamp DESC LIMIT :limit', array(':limit'=> 10));
 	}
 	public function getPostsbyUser($userID) {
-		return $this->db->select('SELECT LEFT(blogPosts.postText, 60) as postSummary, blogPosts.*, users.userName, COUNT(comments.commentID) as noComments 
-			FROM blogPosts 
-			LEFT JOIN users on blogPosts.userID = users.userID 
-			LEFT JOIN comments on blogPosts.postID = comments.postID
-			WHERE blogPosts.userID = :userID
-			GROUP BY blogPosts.postID'
-			, array(':userID' => $userID));
+		return $this->db->select($this->baseQuery . ' WHERE blogPosts.userID = :userID GROUP BY blogPosts.postID', array(':userID' => $userID));
 	}
 
 	public function mostRead($days) {
 		$startTime = strtotime('-' . $days . 'days');
 		$endTime = strtotime('now');
-		$query = 'SELECT LEFT(blogPosts.postText, 60) as postSummary, blogPosts.*, users.userName, COUNT(postViews.postID) as readCount, COUNT(comments.postID) as noComments FROM blogPosts 
-					 LEFT JOIN postViews ON postViews.postID = blogPosts.postID 
-					 LEFT JOIN users ON blogPosts.userID = users.userID
-					 LEFT JOIN comments ON comments.PostID = blogPosts.postID
-					 WHERE blogPosts.timestamp BETWEEN :startTime AND :endTime
-					 GROUP BY blogPosts.postID
-					 ORDER BY readCount DESC'
-					 ;
+		$query = $this->baseQuery . ' WHERE blogPosts.timestamp BETWEEN :startTime AND :endTime
+					 					GROUP BY blogPosts.postID
+					 					ORDER BY readCount DESC';
 
 		return $this->db->select($query, array(':startTime' => $startTime, ':endTime' => $endTime));
 	}
@@ -42,14 +31,10 @@ class IndexModel extends BaseModel {
 	public function mostCommented($days) {
 		$startTime = strtotime('-' . $days . 'days');
 		$endTime = strtotime('now');
-		$query = 'SELECT LEFT(blogPosts.postText, 60) as postSummary, blogPosts.*, users.userName, COUNT(comments.commentID) as noComments FROM blogPosts 
-			LEFT JOIN comments ON comments.postID = blogPosts.postID 
-			LEFT JOIN users ON blogPosts.userID = users.userID
-			WHERE blogPosts.timestamp BETWEEN :startTime AND :endTime
-			GROUP BY blogPosts.postID
-			ORDER BY noComments DESC';
+		$query = $this->baseQuery . ' WHERE blogPosts.timestamp BETWEEN :startTime AND :endTime
+									  GROUP BY blogPosts.postID
+									  ORDER BY noComments DESC';
 
 		return $this->db->select($query, array(':startTime' => $startTime, ':endTime' => $endTime));
-
 	}
 }
