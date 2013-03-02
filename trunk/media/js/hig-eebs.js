4ad28203155726cc6a8f767a297b794402897677
newsflash@gmail.com <newsflash@gmail.com@76afb865-2419-4a4a-d925-d84ee1375376>
2013-03-02 16:35:38 +0000

    Add fb-like buttons. So sloow

--- trunk/media/js/hig-eebs.js
+++ trunk/media/js/hig-eebs.js
@@ -17,4 +17,12 @@ $(document).ready(function() {
 		return false;
 	});*/
 	$(".fancybox").fancybox();
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=288792191246167";
+	   fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 });
