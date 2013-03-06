8afb42d53ab56d23d814ac67480bf4b3de3aa136
ohlaughbrg@gmail.com <ohlaughbrg@gmail.com@76afb865-2419-4a4a-d925-d84ee1375376>
2013-03-06 14:23:40 +0000

    flagged comments

--- trunk/application/views/admin/flaggedComments.php
+++ trunk/application/views/admin/flaggedComments.php
@@ -1,11 +1,35 @@
 <?php
-echo '<pre>';
+echo '<div class="size1 default">';
+echo '<table class="table leftAlign">';
+echo '	<tr>
+			<th>Report</th>
+			<th>Reported by</th>
+			<th>Time</th>
+			<th>link</th>
+		</tr>';
 
 foreach($this->vars['flagged'] as $comment) {
-	//print_r($comment);
 	extract($comment);
+
+
+echo '
+		<tr>
+			<td>' .$reportText. '</td>
+			<td>' .$name. '</td>
+			<td>' .date('d.m.y H:i', $timestamp). '</td>
+			<td>' .HTML::appLink('comments/view/' . $blogName . '/' . $postURL . '/comments/' . $commentID, 'comment'). '
+		</tr>';
+
+
+
+/*
+
 	echo '<p>Report on: ';
 	echo HTML::appLink('comments/view/' . $blogName . '/' . $postURL . '/comments/' . $commentID, 'comment');
 	echo ' Report: ' . $reportText;
 	echo ' Reported by ' . $name . ' at ' . date('d.m.y H:i', $timestamp) . '</p>';
+	*/
 }
+
+echo '</table>';
+echo '</div>';
