c50942f5a283d075a80bd79a72f8cf32f74fe240
bundy.strand <bundy.strand@gmail.com>
2013-03-06 17:00:20 +0000

    VALIDATING LIKE BAOSSES (atleast abit as baosses)

--- application/helpers/ValidateForm.php
+++ application/helpers/ValidateForm.php
@@ -12,7 +12,12 @@ class ValidateForm {
 
 	public function setRequired($fields) {
 		$this->requiredFields = $fields;
+
+		
 	}
+	
+
+
 	public function setMinLength($fields) {
 		$this->minLength = $fields;	
 	}
@@ -22,23 +27,45 @@ class ValidateForm {
 	
 	public function check() {
 		if($this->requiredFields !== false) {
-			foreach($this->requiredFields as $field) {
-				if(!isset($this->form[$field])) {
-					array_push($this->errors, $field . ' is not properly filled out');
-				}
-			}
-		}
+			foreach($this->requiredFields as $key => $value) {
+				if(array_key_exists($key, $this->form)) {
+					$this->xss_cleaner($this->form[$key]);
+				
+					if(!isset($this->form[$key])) {
+						array_push($this->errors, $key . ' is not properly filled out');
+					}
+						
+					if(strlen($this->form[$key]) < $value['minLength']) {
+						array_push($this->errors,  $key . ' must be atleast ' . $value['minLength'] . ' characters');
+					}
+				
 
-		if($this->minLength !== false) {
-			foreach($this->minLength as $field => $minLength) {
-				if(strlen($this->form[$field]) < $minLength) {
-					array_push($this->errors,  $field . ' must be atleast ' . $minLength . ' characters');
-				}
+					if(strlen($this->form[$key]) > $value['maxLength']) {
+						array_push($this->errors,  $key . ' must be atleast ' . $value['maxLength'] . ' characters');
+					} 
+						
+					if(array_key_exists('regex', $value)) {
+						print($value['regex']);
+						if(!preg_match($value['regex'], $this->form[$key])) {
+							array_push($this->errors, $key . ' is SHIT ' . $key .  ' characters');
+					}
+
+					}
+					}
 			}
-		}
+				
 		if(count($this->errors) > 0) {
 			return false;
 		}
+		
 		return true;
 	}
+}
+
+	function xss_cleaner($input_str) {
+    	$return_str = str_replace( array('<','>',"'",'"',')','('), array('&lt;','&gt;','&apos;','&#x22;','&#x29;','&#x28;'), $input_str );
+    	$return_str = str_ireplace( '%3Cscript', '', $return_str );
+    	return $return_str;
+}	
+
 }
